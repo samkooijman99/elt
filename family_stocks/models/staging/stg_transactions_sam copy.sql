7f2226372363a6b81ec69efee1aad6137f5f13a2
samleren1 <samkooijman@live.nl>
2025-01-30 23:58:27 +0100

    first staging model!

---
+++ family_stocks/models/staging/stg_transactions_sam copy.sql
@@ -0,0 +1,42 @@
+with clean_columns as (
+
+select 
+    "Datum" as datum, 
+    "Tijd" as tijd,
+    "Product" as product,
+    "ISIN" as isin,
+    "Beurs" as beurs,
+    "Uitvoeringsplaats" as uitvoeringsplaats,
+    "Aantal" as aantal,
+    "Koers" as koers,
+    i as valuta,
+    "Lokale waarde" as lokale_waarde, 
+    "Waarde" as waarde,
+    "Wisselkoers" as wisselkoers,
+    "Transactiekosten en/of" as transactiekosten,
+    "Totaal" as totale_bedrag,
+    "Order ID" as order_id
+from 
+    {{ ref('transactions_sam') }}
+
+)
+
+select 
+    datum,
+    tijd,
+    product,
+    isin,
+    beurs,
+    uitvoeringsplaats,
+    aantal,
+    koers,
+    valuta,
+    abs(lokale_waarde) as lokale_waarde,
+    abs(waarde) as waarde,
+    wisselkoers
+    transactiekosten,
+    abs(totale_bedrag) as totale_bedrag,
+    order_id,
+    case when waarde < 0 then 'aankoop' else 'verkoop' end as transactietype
+from 
+    clean_columns
