dd6cf9b8947daf81a2955bd323f98b2d1fdef973
samleren1 <samkooijman@live.nl>
2025-02-06 17:36:20 +0100

    filter out single rows

--- family_stocks/models/intermediate/value_over_time.sql
+++ family_stocks/models/intermediate/value_over_time.sql
@@ -1,14 +1,29 @@
+WITH sum_some_shit as (
+    select 
+        date,
+        symbol,
+        sum(total_value) over (partition by symbol order by date asc) as current_costs,
+        sum(quantity) over (partition by symbol order by date asc) as current_quantity,
+        close
+    from 
+        {{ ref ('yfinance_x_stocks')}}
+    order by date asc
+),
 
-with sum_some_shit as (select 
-    date,
-    symbol,
-    sum(total_value) over (partition by symbol order by date asc) as current_costs,
-    sum(quantity) over (partition by symbol order by date asc) as current_quantity,
-    close
-from 
-    {{ ref ('yfinance_x_stocks')}}
+calculate_current_value as (
+    select *, 
+        close * current_quantity as current_value
+    from sum_some_shit
+    where current_costs > 0
+),
 
-order by date asc)
+correct_dates as (
+    select date 
+    from calculate_current_value 
+    group by date
+    having count(*) = 2
+)
 
-select *, close * current_quantity as current_value
-from sum_some_shit
+select *
+from calculate_current_value
+where date in (select date from correct_dates)
