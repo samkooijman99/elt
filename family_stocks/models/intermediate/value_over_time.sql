3fa38bccd947f31264d0dc453d555d3521e33f28
samleren1 <samkooijman@live.nl>
2025-02-06 21:59:52 +0100

    implement running dividends

--- family_stocks/models/intermediate/value_over_time.sql
+++ family_stocks/models/intermediate/value_over_time.sql
@@ -2,21 +2,28 @@ WITH sum_some_shit as (
     select 
         date,
         symbol,
-        sum(total_value) over (partition by symbol order by date asc) as current_costs,
+        sum(total_value) over (partition by symbol order by date asc) as running_costs,
         sum(quantity) over (partition by symbol order by date asc) as current_quantity,
-        dividends * sum(quantity) over (partition by symbol order by date asc) as dividend_payout,
+        dividends,
         close
     from 
         {{ ref ('yfinance_x_stocks')}}
     order by date asc
 ),
 
+calculate_dividends as (
+    select *,
+    dividends * current_quantity as dividend_payout,
+    sum(dividends * current_quantity) over (partition by symbol order by date asc) as running_dividend_payouts
+    from sum_some_shit
+),
+
 calculate_current_value as (
     select 
         *, 
         close * current_quantity as current_value
-    from sum_some_shit
-    where current_costs > 0
+    from calculate_dividends
+    where running_costs > 0
 ),
 
 correct_dates as (
@@ -27,7 +34,7 @@ correct_dates as (
 )
 
 select *,
-current_value - current_costs as current_profit
+current_value - (running_costs - running_dividend_payouts) as current_profit
 from calculate_current_value
 where 
 date in (select date from correct_dates)
